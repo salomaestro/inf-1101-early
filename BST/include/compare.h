71093248e2f566495c7e440affeffbede23ff15a
salomaestro <chris10an.salomonsen@gmail.com>
2023-01-22 21:54:22 +0100

    more comparison functions

--- BST/include/compare.h
+++ BST/include/compare.h
@@ -1,8 +1,12 @@
 #ifndef COMPARE_H
 #define COMPARE_H
 
+#include <string.h>
+
 typedef int (*cmpfunc_t) (void *, void*);
 
 int cmp_int(void *arg1, void *arg2);
+int cmp_char(void *arg1, void *arg2);
+int cmp_str(void *arg1, void *arg2);
 
 #endif
