471a970a2b8b7031e24ccbae5cf27a6c3181adb6
salomaestro <chris10an.salomonsen@gmail.com>
2023-01-22 21:54:28 +0100

    additional tests

--- BST/src/main.c
+++ BST/src/main.c
@@ -4,31 +4,52 @@
 #include "binarysearch.h"
 #include "compare.h"
 
+void test_int() {
+    cmpfunc_t cmp = cmp_int;
+    print_func print_int = print_integer;
+
+    int my_data[] = {2, 3, 5, 1, 8, 9, 7, 0, 4, 6, 5};
+    int data_len = 11;
+
+    tree_t *t = tree_create(cmp, my_data, data_len, sizeof(int), 2);
+
+    tree_print(t, print_int);
+
+    tree_destroy(t);
+}
+
+void test_char() {
+    cmpfunc_t cmp = cmp_char;
+    print_func print = print_character;
+
+    char my_data[] = "Helloworld!=?";
+    int data_len = 13;
+
+    tree_t *t = tree_create(cmp, my_data, data_len, sizeof(char), 2);
+
+    tree_print(t, print);
+
+    tree_destroy(t);
+}
+
+void test_str() {
+    cmpfunc_t cmp = cmp_str;
+    print_func print = print_string;
+
+    char my_data[7][10] = {"Hello", "world", "I", "am", "finally",
+                           "achieving", "something!"};
+    // int data_len = 10;
+    tree_t *t = tree_create(cmp, my_data, 7, sizeof(char) * 10, 4);
+
+    tree_print(t, print);
+
+    tree_destroy(t);
+}
+
 int main()
 {
-    cmpfunc_t cmp = cmp_int;
-    tree_t *tree = tree_create(cmp);
-
-    time_t t;
-    srand((unsigned) time(&t));
-
-    for (int i = 0; i < 25; i++) {
-
-        int *inp = malloc(sizeof(int));
-        *inp = rand() % 25;
-    
-        tree_node_t *node;
-        node = tree_createnode(inp);
-
-        tree_insert(tree, node);
-    }
-
-    print_func print_int = print_integer;
-    print_func print_char = print_character;
-
-    tree_print(tree, print_int);
-
-    tree_destroy(tree);
-    
+    // test_int();
+    // test_char();
+    // test_str();
     return 0;
 }
